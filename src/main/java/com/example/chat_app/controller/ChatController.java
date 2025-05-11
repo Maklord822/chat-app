0274bc60a64866327852112770f54cd82ad7f239
Abdullah Mohamed <140980057+Maklord822@users.noreply.github.com>
2025-05-11 22:24:24 +0300

    Update ChatController.java

--- src/main/java/com/example/chat_app/controller/ChatController.java
+++ src/main/java/com/example/chat_app/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class ChatController {
@@ -12,6 +13,7 @@ public class ChatController {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
+    @CrossOrigin
     @MessageMapping("/chat.send") // mapped to /app/chat.send
     public void sendMessage(Message message) {
         messagingTemplate.convertAndSend("/topic/messages", message);
